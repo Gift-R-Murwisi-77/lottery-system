fa8f354edf917831fa660ab332087a416bdb11a6
Gift-77 <160131458+Gift-R-Murwisi-77@users.noreply.github.com>
2025-02-11 21:40:06 +0200

    connecting with the database using connect

--- script.js
+++ script.js
@@ -126,3 +126,20 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }, 1500);
   });
+
+  const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://lottoStar:<LottoStar123>@cluster0.nz77a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
